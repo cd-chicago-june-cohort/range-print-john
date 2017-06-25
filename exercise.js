fd4596fbdcd709eb0a24987323bbbac4ac543503
JP Doherty <jpdoherty90@gmail.com>
2017-06-24 19:04:38 -0500

    more bonus functionality

--- exercise.js
+++ exercise.js
@@ -1,10 +1,10 @@
 function printRange (start, end, skip) {
-    if (!skip) {
-        skip = 1;
-    }
-    if (!end) {
+    if (! skip && !end) {
         end = start;
         start = 0;
+        skip = 1;
+    } else if (!skip) {
+        skip = 1;
     }
     for (var i = start; i < end; i += skip) {
         console.log(i);
